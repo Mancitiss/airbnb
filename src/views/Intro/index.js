d545a390d403ce9d7badd1ff8f7ae5b1390c94e2
Mancitiss <vovuongthanhtu@gmail.com>
2023-01-02 21:12:34 +0700

    feat/intro

---
+++ src/views/Intro/index.js
@@ -0,0 +1,41 @@
+// this is intro of the app
+// it only has an image at the center of the app
+// it is used to show the user that the app is loading
+
+import React from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+
+const Intro = ({navigation}) => {
+    // make the app load for 10 seconds while showing Intro
+    // and then navigate to Login screen
+    setTimeout(() => {
+      navigation.navigate('Login');
+    }, 10000);
+    
+    return (
+        <View style={styles.container}>
+        <Image
+            style={styles.image}
+            source={require('../../../assets/logo-white.png')}
+        />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#A38F8B'
+    },
+    image: {
+      width: '100%',
+      height: '100%',
+      maxWidth: 500,
+      maxHeight: 500,
+      resizeMode: 'contain',
+    }
+});
+
+export default Intro;
